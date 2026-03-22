ccc4cd48fda4b4f243d81084578f9fe8c4b89abe
BhanuBilla24 <bhanubilla24@gmail.com>
2026-03-22 20:55:48 +0530

    Create test.groovy

---
+++ vars/test.groovy
@@ -0,0 +1,11 @@
+def call() {
+  if (params.env=="main") {
+    sh echo "testing in main"
+  }
+  else if (params.env=="dev") {
+    sh echo "testing in dev"
+  }
+  else {
+    sh echo "testing in qa"
+  }
+}
