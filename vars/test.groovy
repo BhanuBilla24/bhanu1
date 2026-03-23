845715998cf20f0b0ef55255fb485553eb77cd40
BhanuBilla24 <bhanubilla24@gmail.com>
2026-03-23 14:05:24 +0530

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,11 +1,11 @@
 def call(env) {
   if (params.env=="main") {
-    sh echo "testing in main"
+    step.sh "echo testing in main"
   }
   else if (params.env=="dev") {
-    sh echo "testing in dev"
+    step.sh "echo testing in dev"
   }
   else {
-    sh echo "testing in qa"
+    step.sh "echo testing in qa"
   }
 }
